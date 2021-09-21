6d32eb39705ee7afcd44e8ae4a869a821d7dbba7
Peter Boersma <peter@growingminds.nl>
2021-09-21 23:01:45 +0200

    Change

--- includes/WP_ntwcwppi.php
+++ includes/WP_ntwcwppi.php
@@ -7,24 +7,28 @@ class WP_ntwcwppi
   public function __construct($plugin)
   {
    $this->plugin = $plugin;
-    // Register API Route for saving Authorization Token.
-    add_action('rest_api_init', function () {
-      register_rest_route('ntwcwppi/v1', '/authorized', array(
-        'methods' => 'POST',
-        'callback' => array($this, 'ntwcwppi_saveAuthorization'),
-      ));
-    });
-    
   }
 
   public function run()
   {
-    register_activation_hook($this->plugin, array($this, 'ntwcwppi_createAuthTokens'));
+
+    register_activation_hook($this->plugin, array($this, 'setup'));
 
    
 
     // // CRUD Menu for Product Importer UI
     // //add_action( 'admin_menu', 'extra_post_info_menu' );
+  }
+
+  public function setup()
+  {
+     // Register API Route for saving Authorization Token.
+     add_action('rest_api_init', function () {
+      register_rest_route('ntwcwppi/v1', '/authorized', array(
+        'methods' => 'POST',
+        'callback' => array($this, 'ntwcwppi_saveAuthorization'),
+      ));
+    });
   }
 
   public function ntwcwppi_createAuthTokens()
