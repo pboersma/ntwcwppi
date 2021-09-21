7323c68f3a9b68ef686e1472880de4449ef7b37b
Peter Boersma <p.boersma@lycan-media.nl>
2021-09-21 19:59:31 +0000

    Start

---
+++ ntwcwpproductimporter.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * NT Woocommerce Wordpress Product Importer
+ *
+ * @package           wcwppi
+ * @author            Peter Boersma
+ * @copyright         2021 Peter Boersma
+ *
+ * @wordpress-plugin
+ * Plugin Name:       NT Woocommerce Wordpress Product Importer
+ * Description:       Product Import Plugin for Woocommerce to allow auto-importation of products.
+ * Version:           1.0.0
+ **/
+
+require 'vendor/autoload.php';
+//require 'includes/WP_ntwcwppi.php';
+
+//$plugin = new WP_ntwcwppi();
+//$plugin->run();
