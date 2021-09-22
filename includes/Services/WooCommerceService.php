49647e89350c0ee839b810b5af1d7d897e9eb4b6
Peter Boersma <peter@growingminds.nl>
2021-09-22 23:58:06 +0200

    Change

---
+++ includes/Services/WooCommerceService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Services;
+
+
+use Automattic\WooCommerce\Client;
+
+
+class WooCommerceService {
+
+    private $credentials;
+    private $client;
+
+    public function __construct(object $credentials)
+    {
+        $this->credentials = $credentials;
+
+        $this->client = new Client(
+            "https://$_SERVER[HTTP_HOST]",
+            $credentials['consumer_key'],
+            $credentials['consumer_secret'],
+            [
+                'wp_api' => true,
+                'version' => 'wc/v3'
+            ]
+        );
+    }
+
+    public function listAllProducts()
+    {
+        $this->client->get('products');
+    }
+
+}
