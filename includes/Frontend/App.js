25de651f8054cb43064a4f82cd091e9ed7b725f6
Peter Boersma <peter@growingminds.nl>
2021-10-12 22:32:41 +0200

    Test

--- includes/Frontend/App.js
+++
@@ -1,8 +0,0 @@
-( function() {
-    var vm = new Vue({
-      el: document.querySelector('#ntwcwppi_app'),
-      mounted: function(){
-        console.log("Hello Vue!");
-      }
-    });
-  })();  
